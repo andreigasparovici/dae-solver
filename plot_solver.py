31cab0f2eadf6f5dd913d085828c75d05979f6ae
Andrei Gasparovici <andreigasparovici1@gmail.com>
2020-09-25 11:26:03 +0300

    Updated solver result plotting script

--- plot_solver.py
+++ plot_solver.py
@@ -2,14 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 fig, ax = plt.subplots(1, 2, sharey=True)
 
 fig.set_size_inches(22, 10)
 
-for i in range(1, 6):
-    df = pd.read_csv(f'solver-{i}.csv')
+iterations = 5
 
+for i in range(1, iterations + 1):
+    df = pd.read_csv(f'solver-{i}.csv')
     rows, cols = df.shape
 
     t0, t = i, i+1
@@ -19,11 +19,11 @@ for i in range(1, 6):
 
     for j, col in enumerate(df.columns):
         y = df[col].to_numpy()
-        ax[j].plot(X, y, label=f'{col} {i}', marker='.')
+        ax[j].plot(X, y, label=str(i), marker='.')
         ax[j].set_xlabel('t')
+        ax[j].set_ylabel(col)
 
 for j in range(2):
     ax[j].legend()
 
-plt.savefig('solver-out.png')
-
+plt.savefig('solver.png')
